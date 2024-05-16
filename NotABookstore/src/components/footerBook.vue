506ca4bcbf3e872181b7c5f11dc249e2ddb14e47
Cachedd <103432514+Cachedd@users.noreply.github.com>
2024-05-16 11:15:10 +1000

    feat: Changes to footer

--- NotABookstore/src/components/footerBook.vue
+++ NotABookstore/src/components/footerBook.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="py-5 mt-5">
         <div class="container-fluid text-center bg-dark fixed-bottom">
-            <p class="mb-3 mt-3 text-white"> &copy; MustReads 2024</p>
+            <p class="mb-3 mt-3 text-white"> Made with ❤️ from Australia</p>
         </div>
     </footer>
 </template>
